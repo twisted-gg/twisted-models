05a3282f6e1a1f39193fe746d3f8434f423a4a46
Sansossio <juliosansossio@gmail.com>
2019-12-05 19:19:34 +0100

    Update version

--- src/tft-matches/match/tft-match.dto.ts
+++ src/tft-matches/match/tft-match.dto.ts
@@ -1,8 +1,9 @@
 import { Regions } from '@twisted.gg/common/dist/wrapper/constants'
-import { ApiResponseModelProperty } from '@nestjs/swagger'
+import { ApiResponseModelProperty, ApiModelProperty } from '@nestjs/swagger'
 import { QueueDTO } from '../../static'
 import { TftMatchParticipantsModelDto } from '../participants'
 
+// Match details
 export class TftMatchModelDTO {
   @ApiResponseModelProperty()
   match_id!: string
@@ -21,7 +22,7 @@ export class TftMatchModelDTO {
   @ApiResponseModelProperty()
   game_length!: number
 
-  @ApiResponseModelProperty()
+  @ApiResponseModelProperty({ type: QueueDTO })
   queue!: QueueDTO
 
   @ApiResponseModelProperty()
@@ -38,4 +39,42 @@ export class TftMatchModelDTO {
   @ApiResponseModelProperty()
   participantsIds!: string[]
 
+}
+
+// Listing
+
+export class TftMatchModelDTOListingMatches {
+  @ApiResponseModelProperty()
+  _id!: string
+
+  @ApiResponseModelProperty()
+  datetime!: Date
+
+  @ApiResponseModelProperty()
+  version!: string
+
+  @ApiResponseModelProperty()
+  length!: number
+
+  @ApiResponseModelProperty({ type: QueueDTO })
+  queue!: QueueDTO
+
+  @ApiResponseModelProperty({ type: TftMatchParticipantsModelDto })
+  summoner!: TftMatchParticipantsModelDto
+}
+
+export class TftMatchModelDTOListing {
+  @ApiResponseModelProperty()
+  page!: number
+
+  @ApiResponseModelProperty()
+  limit!: number
+
+  @ApiResponseModelProperty()
+  total!: number
+
+  @ApiResponseModelProperty({
+    type: [TftMatchModelDTOListingMatches]
+  })
+  data!: TftMatchModelDTOListingMatches[]
 }
